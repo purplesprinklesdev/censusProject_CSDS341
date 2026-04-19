f4bdbaf0061261118b70b8bc2de32959bc1f63c4
purplesprinklesdev <ought-impale-doozy@duck.com>
2026-04-19 19:10:32 -0400

    api, sql schema updates

--- sql/insertIntoPerson.sql
+++ sql/insertIntoPerson.sql
@@ -1,2 +1,5 @@
-INSERT INTO Person (Serial_No, Person_ID, PUMA, Person_Weight, Sex, Race, Ethnicity, Age, Educational_Attainment, Marital_Status, Employment_Status, State)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+INSERT INTO Person (Serial_No, Person_ID, PUMA, Person_Weight, Sex, Race,
+Ethnicity, Age, Educational_Attainment, Marital_Status, Health_Insurance,
+Employment_Status, Weeks_Worked, Multilingual, English_Ability,
+Decade_of_Immigration, Citizenship_Status, State)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
