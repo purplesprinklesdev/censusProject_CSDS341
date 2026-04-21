92a3e0d6d05f09b6555d8b9351f2bc3c3aa2214e
atrib23 <150200426+atrib23@users.noreply.github.com>
2026-04-21 19:13:42 -0400

    Display PUMA names for topPumas

--- sql/topPumas.sql
+++ sql/topPumas.sql
@@ -1,5 +1,8 @@
-SELECT PUMA, SUM(Person_Weight) AS estimated_population
-FROM Person
-GROUP BY PUMA
+SELECT SUM(p.Person_Weight) AS estimated_population, pu.name
+FROM Person p
+LEFT JOIN PUMA pu
+    ON p.PUMA = pu.PUMA
+    AND p.State = pu.State
+GROUP BY p.PUMA, p.State
 ORDER BY estimated_population DESC
 LIMIT 10
