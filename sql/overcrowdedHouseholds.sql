91d1a00b423cecb427ea08a1defa5cd1b1c879e6
AssumedHydra <mohit.r.nair@gmail.com>
2026-04-27 11:03:30 -0400

    Update overcrowdedHouseholds.sql
    Debugged this, SQL error shouldn't occur now

--- sql/overcrowdedHouseholds.sql
+++ sql/overcrowdedHouseholds.sql
@@ -1,5 +1,5 @@
 SELECT
-    PUMA
+    h.PUMA,
     SUM(h.Household_Weight)                       AS Weighted_Qualifying_Households,
     ROUND(CAST(SUM(h.Household_Income * h.Household_Weight) AS REAL) /
           NULLIF(SUM(h.Household_Weight), 0), 2)  AS Weighted_Avg_Income,
@@ -7,9 +7,9 @@ SELECT
           NULLIF(SUM(h.Household_Weight), 0), 2)  AS Weighted_Avg_Children
 FROM Household h
 WHERE
-    Household_Children  >= 3
-    AND Workers_in_Family   <= 1
-    AND Household_Income BETWEEN 1 AND 40000
-    AND Tenure IN (2, 3)
-GROUP BY PUMA
+    h.Household_Children  >= 3
+    AND h.Workers_in_Family   <= 1
+    AND h.Household_Income BETWEEN 1 AND 40000
+    AND h.Tenure IN (2, 3)
+GROUP BY h.PUMA
 ORDER BY Weighted_Qualifying_Households DESC;
