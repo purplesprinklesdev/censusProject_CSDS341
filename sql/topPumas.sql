1623849f03a050c4772bff102f8b348e1be49e25
AssumedHydra <mohit.r.nair@gmail.com>
2026-04-27 11:22:00 -0400

    Update topPumas.sql
    Updated Header

--- sql/topPumas.sql
+++ sql/topPumas.sql
@@ -1,8 +1,10 @@
-SELECT SUM(p.Person_Weight) AS estimated_population, pu.name
+SELECT 
+    pu.name AS PUMA_Name,
+    SUM(p.Person_Weight) AS estimated_population
 FROM Person p
 LEFT JOIN PUMA pu
     ON p.PUMA = pu.PUMA
     AND p.State = pu.State
-GROUP BY p.PUMA, p.State
+GROUP BY p.PUMA, p.State, pu.name
 ORDER BY estimated_population DESC
 LIMIT 10
